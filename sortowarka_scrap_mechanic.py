59e34e2b8c1c129f0f2392e25405be559242cd9f
Adam Henke <henkeadam51@gmail.com>
2025-01-12 21:33:50 +0100

    21:35 12.1.2025

--- sortowarka_scrap_mechanic.py
+++ sortowarka_scrap_mechanic.py
@@ -1,5 +1,6 @@
 import os
 import time
+from time import sleep
 from datetime import datetime
 import pathlib
 import shutil
@@ -64,7 +65,57 @@ def mk_dir():
     except:
         isExist == True
 
+def move_to_work_folder():
+    work_folder = os.path.join(rootdir, "0000Blueprints_work")
+    if not os.path.exists(work_folder):
+        os.mkdir(work_folder)
+    print(f"Work folder location: {work_folder}")
+
+    folders = [os.path.join(rootdir, folder) for folder in os.listdir(rootdir) if os.path.isdir(os.path.join(rootdir, folder))]
+    
+    for folder in folders:
+        folder_name = os.path.basename(folder)
+        if folder_name != "0000Blueprints_work":
+            target_path = os.path.join(work_folder, folder_name)
+            try:
+                shutil.move(folder, target_path)
+                print(f"Moved {folder} to {target_path}")
+
+                temp_file_path = os.path.join(target_path, "temp_file.txt")
+                with open(temp_file_path, 'w') as temp_file:
+                    temp_file.write("") 
+                print(f"Created temporary file: {temp_file_path}")
+
+                os.remove(temp_file_path)
+                print(f"Deleted temporary file: {temp_file_path}")
+
+            except Exception as e:
+                print(f"Failed to move {folder}: {e}")
+        sleep(0.25)
+
+def move_back_to_main_folder():
+    work_folder = os.path.join(rootdir, "0000Blueprints_work")
+    if not os.path.exists(work_folder):
+        print(f"Work folder {work_folder} does not exist. Nothing to move.")
+        return
+
+    folders = [os.path.join(work_folder, folder) for folder in os.listdir(work_folder) if os.path.isdir(os.path.join(work_folder, folder))]
+    sorted_folders = sorted(folders, key=get_latest_mod_time, reverse=True)
+
+    for folder in sorted_folders:
+        folder_name = os.path.basename(folder)
+        target_path = os.path.join(rootdir, folder_name)
+        try:
+            shutil.move(folder, target_path)
+            print(f"Moved {folder} back to {target_path}")
+        except Exception as e:
+            print(f"Failed to move {folder}: {e}")
+        sleep(0.01)
+
 mk_dir()
 Walkthrough()
+move_to_work_folder()
+move_back_to_main_folder()
 print_sorted()
+Walkthrough()
 count_blueprints()
